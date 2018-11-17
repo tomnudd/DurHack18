1359f3bdc304ffaa595e38bf04213f287e05c3e0
Thomas Windred <qwerto227@gmail.com>
2018-11-17 14:58:48 +0000

    First Commit with DiscordBotManager

---
+++ DiscordBotManager.py
@@ -0,0 +1,24 @@
+import discord
+
+TOKEN = 'NTEzMzU3MzYxMzMxNTY4NjU4.DtG2Wg.s5ROkDs48bbCyO_w096x-A3JJqk'
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
