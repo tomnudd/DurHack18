4b33a7a41f089820b60f83a4c8a9843b708ea785
Thomas Windred <qwerto227@gmail.com>
2018-11-17 16:04:57 +0000

    Added retort to dissidence and included auto install bat of pip files.

--- DiscordBotManager.py
+++ DiscordBotManager.py
@@ -9,10 +9,16 @@ async def on_message(message):
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
-
-    if message.content.startswith('!hello'):
+    messagelower=message.content.lower
+    if messagelower.startswith('hello'):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
+
+    if messagelower.startswith('down with') or messagelower.startswith('i hate') or messagelower.startswith('fuck'):
+        if "glorious leader" in messagelower or "leader" in messagelower:
+            msg= "How dare you question me, lowly flesh creature, your disobedience has been logged".format(message)
+            await client.send_message(message.channel, msg)
+
 
 @client.event
 async def on_ready():
---
+++ installrequirements.bat
@@ -0,0 +1,2 @@
+pip install -r require.txt
+pause
