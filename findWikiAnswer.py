8c9ea431b42b7b983c33805e7e1c3a6493010f23
Meredith Gibbons <mgibbons1705@yahoo.co.uk>
2018-11-17 18:02:29 +0000

    Now calculates if a given input is blackmail-worthy

--- findWikiAnswer.py
+++ findWikiAnswer.py
@@ -1,7 +1,18 @@
 import nltk
 import wikipedia
 
+blackmailWords = {"sex", "masturbation", "murder", "minecraft", "serial killer", "fetish", "leather", "kink", "cult"}
+blackmailDict = {}
+
 def askQuestion(string):
+    """
+    :param string:
+    :returns a tuple of a list of wikipedia summaries releavant to the question and a boolean indicating whether the question can be used as blackmail:
+    """
+    isBlackmail = False
+    blackmailDict.clear()
+    for word in blackmailWords:
+        blackmailDict.update({word:0})
     adviceLst = list()
 
 # adapted from user Boa on https://stackoverflow.com/questions/33587667/extracting-all-nouns-from-a-text-file-using-nltk
@@ -13,9 +24,27 @@ def askQuestion(string):
     for noun in nouns:
             title = wikipedia.search(noun, 1, True)
             if title[0] is not None:
-                adviceLst.append(wikipedia.summary(title))
-    print(adviceLst)
-    return adviceLst
+                adviceLst.append(wikipedia.summary(title[0]))
+                page = wikipedia.WikipediaPage(title[0])
+                findBlackmail(page.content)
+
+    for word in blackmailWords:
+        if blackmailDict[word] > len(nouns) *3:
+            isBlackmail = True
+    return [adviceLst, isBlackmail]
+
+def findBlackmail(content):
+    # adapted from user Boa on https://stackoverflow.com/questions/33587667/extracting-all-nouns-from-a-text-file-using-nltk
+    # function to test if something is a noun
+    is_noun = lambda pos: pos[:2] == 'NN'
+    # do the nlp stuff
+    tokenized = nltk.word_tokenize(content)
+    nouns = [word for (word, pos) in nltk.pos_tag(tokenized) if is_noun(pos)]
+
+    for noun in nouns:
+        for word in blackmailWords:
+            if word == noun.lower():
+                blackmailDict[word] += 1
 
 def test():
-    askQuestion("The world is ugly, but you're beautiful to me. The cat is cute.")
+    print(askQuestion("Am I ready to have sex?"))
